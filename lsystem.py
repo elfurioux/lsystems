524b64b00e9c51c1373e640fca0b1e409e404f6e
❊ elfurioux ❊ <60098198+elfurioux@users.noreply.github.com>
2024-02-27 19:29:10 +0100

    Updated lsystem.py

--- lsystem.py
+++ lsystem.py
@@ -1,14 +1,16 @@
 
 import turtle
 
-def turtle_setup():
+def turtle_setup(title: str):
     """Turtle initialisation procedure, put the turtle at the bottom of the window,
     in the middle, facing upwards, ready to draw a tree for example."""
+    turtle.mode("logo")
+    turtle.title(titlestring=title)
+
     turtle.penup()
-    turtle.goto(0, -turtle.window_height()//2-10)
+    turtle.goto(0, -turtle.window_height()//2-5)
     turtle.pendown()
 
-    turtle.setheading(90)
     turtle.speed(0)
 
 def turtle_end():
@@ -16,50 +18,69 @@ def turtle_end():
     turtle.hideturtle()
     turtle.done()
 
+
 def draw_instructions(instructions: str, lenght: float, angle: float):
     """Main drawing function, takes a string of instructions and draws it on the screen,
     WARNING: The function just draw the rules set, it does not initialize nor end the turtle,
-    use `turtle_setup()` and `turtle_end()` procedures for this."""
-    ALLOWED_INSTRUCTIONS = "FXY+-&^<>|[]"
+    use `turtle_setup()` and `turtle_end()` procedures for this.
+    TODO: le reste des instructions"""
+
+    NUMBERS = "0123456789"
+    LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ALLOWED_INSTRUCTIONS = "+-&^<>|[]" + LETTERS + LETTERS.lower() + NUMBERS
 
     stack: list[turtle.Vec2D] = []
 
-    for instruction in instructions:
+    for i, instruction in enumerate(instructions):
         if instruction not in ALLOWED_INSTRUCTIONS:
             continue
         
-        if instruction in "FXY": # Se déplacer d’un pas unitaire (∈ V) ;
+        # print(f"{i}: {instruction}")
+
+        ### MOVEMENT INSTRUCTIONS
+        if instruction.upper() in LETTERS or instruction in NUMBERS: # Se déplacer d’un pas unitaire (∈ V) ;
             turtle.forward(lenght)
+        
+        ### ROTATION INSTRUCTIONS
+        elif instruction == "|": # Tourner sur soi-même de 180° (∈ S) ;
+            turtle.left(180)
         elif instruction == "+": # Tourner à gauche d’angle α (∈ S) ;
-            turtle.left(angle)
-        elif instruction == "-": # Tourner à droite d’un angle α (∈ S) ;
             turtle.right(angle)
+        elif instruction == "-": # Tourner à droite d’un angle α (∈ S) ;
+            turtle.left(angle)
+        
+        elif instruction == "<": # Roulez vers la gauche d’un angle α (∈ S) ;
+            raise Exception("Not Implemented")
+            # turtle.setheading(angle)
+            # print("<: angle=",turtle.heading())
+        elif instruction == ">": # Roulez vers la droite d’un angle α (∈ S) ;
+            raise Exception("Not Implemented")
+            # turtle.setheading(360-angle)
+            # print(">: angle=",turtle.heading())
         elif instruction == "&": # Pivoter vers le bas d’un angle α (∈ S) ;
             raise Exception("Not Implemented")
         elif instruction == "^": # Pivoter vers le haut d’un angle α (∈ S) ;
             raise Exception("Not Implemented")
-        elif instruction == "<": # Roulez vers la gauche d’un angle α (∈ S) ;
-            raise Exception("Not Implemented")
-        elif instruction == ">": # Roulez vers la droite d’un angle α (∈ S) ;
-            raise Exception("Not Implemented")
-        elif instruction == "|": # Tourner sur soi-même de 180° (∈ S) ;
-            turtle.left(180)
+        
+        ### SAVE/RESTORE INSTRUCTIONS
         elif instruction == "[": # Sauvegarder la position courante (∈ S) ;
-            stack.append(turtle.pos())
+            stack.append((turtle.pos(),turtle.heading()))
         elif instruction == "]": # Restaurer la dernière position sauvée (∈ S).
-            pos = stack.pop()
+            pos, hd = stack.pop()
             turtle.penup()
             turtle.goto(pos[0],pos[1])
+            turtle.setheading(hd)
             turtle.pendown()
 
 class LSystem:
-    def __init__(self, axiom: str, rules: dict[str], angle: float, lenght: float) -> None:
+    def __init__(self, axiom: str, rules: dict[str], angle: float, lenght: float, title: str = "") -> None:
         self.axiom: str         = axiom
         self.rules: dict[str]   = rules
         self.angle: float       = angle
         self.lenght: float      = lenght
+        self.title: str         = title
 
-    # L SYTEM GENERATION
+    ### L SYTEM GENERATION
 
     def geniterations(self, iterations: int) -> list[str]:
         """Generate a certain number of iterations of the rules set, then returns
@@ -83,11 +104,12 @@ class LSystem:
 
     # L SYSTEM REPRESENTATION
 
-    def draw_lsyst(self, iterations: int) -> None:
-        """Main drawing function, takes in parameter a set of rules (`self`)
+    def draw_lsyst(self, iterations: int, baseangle = 0) -> None:
+        """Main draturtle_setupwing function, takes in parameter a set of rules (`self`)
         and a number of `iterations` of the rules to draw."""
         
-        turtle_setup()
+        turtle_setup(self.title + f" G:{iterations}; L:{self.lenght}; A:{self.angle}")
+        turtle.setheading(baseangle)
         draw_instructions(
             instructions = self.getiteration(iterations),
             lenght = self.lenght,
@@ -95,5 +117,4 @@ class LSystem:
         )
         turtle_end()
     
-    # TODO: LSYSTEM USER INPUT
-
+    ### TODO: LSYSTEM USER INPUT
